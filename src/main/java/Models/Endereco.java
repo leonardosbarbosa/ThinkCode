6de595855d37a31f99f29db71a555c0364130ff2
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2020-04-26 19:22:57 -0300

    Criação de algumas models

---
+++ src/main/java/Models/Endereco.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Leonardo Silva
+ */
+public class Endereco {
+
+    private int id;
+    private int idUsuario;
+    private String cep;
+    private String rua;
+    private String bairro;
+    private String numero;
+    private String complemento;
+    private String dataExclusao;
+    private String userExclusao;
+
+    public Endereco(int id, int idUsuario, String cep, String rua, String bairro, String numero, String complemento) {
+        this.id = id;
+        this.idUsuario = idUsuario;
+        this.cep = cep;
+        this.rua = rua;
+        this.bairro = bairro;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getDataExclusao() {
+        return dataExclusao;
+    }
+
+    public void setDataExclusao(String dataExclusao) {
+        this.dataExclusao = dataExclusao;
+    }
+
+    public String getUserExclusao() {
+        return userExclusao;
+    }
+
+    public void setUserExclusao(String userExclusao) {
+        this.userExclusao = userExclusao;
+    }
+
+}
---
+++ src/main/java/com/mycompany/thinkcode/Program.java
@@ -0,0 +1,7 @@
+package com.mycompany.thinkcode;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
