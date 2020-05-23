14601fd930cf3550b1f194f1b1e2390e5ffef43b
Caous <caous.g@gmail.com>
2020-05-23 20:01:16 -0300

    Subida projeto

--- projetoIntegrador3/src/main/java/com/thinkcode/models/RelatorioModel.java
+++ projetoIntegrador3/src/main/java/com/thinkcode/models/RelatorioModel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.thinkcode.models;
 
 /**
@@ -10,18 +5,27 @@ package com.thinkcode.models;
  * @author Gustavo Nascimento
  */
 public class RelatorioModel {
-    
+
     private int idProduto;
     private int idUsuario; // Chave estrangeira
     private int idFilial; // Chave estrangeira
     private String nomeProduto;
     private String qtdProduto;
     private int Valor;
+    private VendaModel venda;
+    private int idVenda;
+    private ProdutoModel produtoModel;
+    private String filialNome;
+    private String usuarioNome;
+    private String cpfCnpj;
+    private int pagamento;
+    private int parcelas;
+    private Double total;
+    private String data;
 
     public RelatorioModel() {
 
     }
-
 
     public int getIdProduto() {
         return idProduto;
@@ -47,5 +51,77 @@ public class RelatorioModel {
         this.idFilial = idFilial;
     }
 
+    public void setProduto(ProdutoModel _produto) {
+        this.produtoModel = _produto;
+    }
+
+    public ProdutoModel getProduto() {
+        return this.produtoModel;
+    }
+
+    public void setIdVenda(int _idVenda) {
+        this.idVenda = _idVenda;
+    }
+
+    public int getIdVenda() {
+        return this.idVenda;
+    }
+
+    public void setfilialNome(String filialNome) {
+        this.filialNome = filialNome;
+    }
+
+    public String getfilialNome() {
+        return this.filialNome;
+    }
+
+    public void setusuarioNome(String usuarioNome) {
+        this.usuarioNome = usuarioNome;
+    }
+
+    public String getusuarioNome() {
+        return this.usuarioNome;
+    }
+
+    public void setcpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getcpfCnpj() {
+        return this.cpfCnpj;
+    }
+
+    public int getpagamento() {
+        return this.pagamento;
+    }
+
+    public void setpagamento(int pagamento) {
+        this.pagamento = pagamento;
+    }
+
+      public int getparcelas() {
+        return this.parcelas;
+    }
+
+    public void setparcelas(int parcelas) {
+        this.parcelas = parcelas;
+    }
     
+      public Double gettotal() {
+        return this.total;
+    }
+
+    public void settotal(Double total) {
+        this.total = total;
+    }
+    
+    public void setdata(String data) {
+        this.data = data;
+    }
+
+    public String getdata() {
+        return this.data;
+    }
+
+
 }
